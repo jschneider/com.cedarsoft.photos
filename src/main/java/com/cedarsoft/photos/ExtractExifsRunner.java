c884a09e8b11a83e7a9c7ad8c138bf172421c6ca
Johannes Schneider <js@cedarsoft.com>
2016-08-24 21:52:07 +0200

    extract exifs runner

---
+++ src/main/java/com/cedarsoft/photos/ExtractExifsRunner.java
@@ -0,0 +1,40 @@
+package com.cedarsoft.photos;
+
+import com.cedarsoft.photos.di.Modules;
+import com.cedarsoft.photos.tools.exif.ExifExtractor;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class ExtractExifsRunner {
+  public static void main(String[] args) throws IOException {
+    System.out.println("Extracting exif files");
+
+    Injector injector = Guice.createInjector(Modules.getModules());
+    ExifExtractor exifExtractor = injector.getInstance(ExifExtractor.class);
+
+    ImageFinder imageFinder = injector.getInstance(ImageFinder.class);
+    imageFinder.find((storage, dataFile) -> {
+      System.out.println("\t\tExtracting exif for " + dataFile);
+      File dir = dataFile.getParentFile();
+      File exifFile = new File(dir, "exif");
+      if (exifFile.exists()) {
+        return;
+      }
+
+      dir.setWritable(true);
+      try (FileInputStream in = new FileInputStream(dataFile); FileOutputStream out = new FileOutputStream(exifFile)) {
+        exifExtractor.extractDetailed(in, out);
+      } finally {
+        dir.setWritable(false);
+      }
+    });
+  }
+}
